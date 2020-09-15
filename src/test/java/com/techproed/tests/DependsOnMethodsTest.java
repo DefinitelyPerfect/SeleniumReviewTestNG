3ad8215601a3c5f792584f9fe9fc516a862ee43b
Osman Onder <honestseller9@gmail.com>
2020-09-15 08:20:14 -0500

    review 7. g

---
+++ src/test/java/com/techproed/tests/DependsOnMethodsTest.java
@@ -0,0 +1,29 @@
+package com.techproed.tests;
+
+import org.testng.annotations.Test;
+
+public class DependsOnMethodsTest {
+
+    @Test
+    public void test01(){  // sisteme giriş testiniz
+        System.out.println("test01");
+    }
+    @Test (dependsOnMethods = "test01")
+    public void test02(){ // sisteme giriş yaptıktan sonraki işlemler
+        System.out.println("test02");
+    }
+    /*  priority ve dependsOnMethods birlikte de kullanilabilir...
+     @Test (priority = 1)
+    public void test01(){  // sisteme giriş testiniz
+        System.out.println("test01");
+    }
+    @Test (priority = 2, dependsOnMethods = "test01")
+    public void test02(){ // sisteme giriş yaptıktan sonraki işlemler
+        System.out.println("test02");
+    }
+    @Test (priority = 3)
+    public void test03(){
+        System.out.println("test03");
+    }
+     */
+}
